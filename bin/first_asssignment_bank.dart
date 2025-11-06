5a1fc24b31fb83f30a81bdd10d187c9e833e829e
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2025-11-06 21:15:00 +0545

    setup

---
+++ bin/first_asssignment_bank.dart
@@ -0,0 +1,5 @@
+import 'package:first_asssignment_bank/first_asssignment_bank.dart' as first_asssignment_bank;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_asssignment_bank.calculate()}!');
+}
---
+++ test/first_asssignment_bank_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_asssignment_bank/first_asssignment_bank.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
